308fe5cecf2c668c307ee8e323e4bfac2f5c1f47
Nick Telenson <ntelenson@gmail.com>
2019-10-06 17:04:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your solution in this file!
+driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+}
